16830a0b4ea1f5583c54a604d3f05e2f9f954c52
Odd E. Ebbesen <oddebb@gmail.com>
2021-03-09 22:19:07 +0100

    Fixed assignment error in quoteshuffle/qs_test.go

--- quoteshuffle/qs_test.go
+++ quoteshuffle/qs_test.go
@@ -15,7 +15,10 @@ func init() {
 }
 
 func TestShuffle(t *testing.T) {
-	qd := New(QFILE)
+	qd, err := New(QFILE)
+	if nil != err {
+		t.Error(err)
+	}
 
 	for i := 0; i < 3; i++ {
 		t.Logf("Quote: %q", qd.Shuffle())
@@ -24,7 +27,10 @@ func TestShuffle(t *testing.T) {
 
 // This is mostly for resetting the quotes file via the go test command easily
 func TestReset(t *testing.T) {
-	qd := New(QFILE)
+	qd, err := New(QFILE)
+	if nil != err {
+		t.Error(err)
+	}
 	t.Logf("%s: %d unused, and %d used quotes", QFILE, qd.Unused(), qd.Used())
 	qd.ResetAndSave()
 	t.Logf("%s: %d unused, and %d used quotes", QFILE, qd.Unused(), qd.Used())

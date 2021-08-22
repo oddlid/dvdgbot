cd65e820d5c645d3a489ca265c126e510ab0e2e8
Odd E. Ebbesen <oddebb@gmail.com>
2021-08-22 19:27:51 +0200

    Added util.go

---
+++ util/util.go
@@ -0,0 +1,47 @@
+package util
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/rs/zerolog/log"
+)
+
+var _log = log.With().Str("package", "util").Logger()
+
+func EnvDefStr(key, fallback string) string {
+	val, found := os.LookupEnv(key)
+	if !found {
+		_log.Warn().
+			Str("func", "EnvDefStr").
+			Str("env_key", key).
+			Str("fallback", fallback).
+			Msg("Undefined, returning fallback")
+		return fallback
+	}
+	return val // might still be empty, if set, but empty in ENV
+}
+
+func EnvDefInt(key string, fallback int) int {
+	val, found := os.LookupEnv(key)
+	if !found {
+		_log.Warn().
+			Str("func", "EnvDefInt").
+			Str("env_key", key).
+			Int("fallback", fallback).
+			Msg("Undefined, returning fallback")
+		return fallback
+	}
+	intVal, err := strconv.Atoi(val)
+	if err != nil {
+		_log.Error().
+			Str("func", "EnvDefInt").
+			Str("env_key", key).
+			Int("fallback", fallback).
+			Str("value", val).
+			Err(err).
+			Msg("Conversion error")
+		return fallback
+	}
+	return intVal
+}

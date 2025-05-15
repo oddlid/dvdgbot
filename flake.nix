87efb981496c44f028d341dea8bda3d973576a87
Odd E. Ebbesen <git@oddware.net>
2025-05-15 15:16:40 +0200

    Changed to another devshell

--- flake.nix
+++ flake.nix
@@ -1,54 +1,61 @@
 {
-  inputs = {
-    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-    devenv.inputs.nixpkgs.follows = "nixpkgs";
-  };
+  description = "A Nix-flake-based Go 1.24 development environment";
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+  outputs = { self, nixpkgs }:
     let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+      goVersion = 24; # Change this to update the whole stack
+
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+          config = {
+            allowUnfree = true;
+            # permittedInsecurePackages = [ "olm-3.2.16" ];
+          };
+        };
+      });
     in
     {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-        devenv-test = self.devShells.${system}.default.config.test;
+      overlays.default = final: prev: {
+        go = final."go_1_${toString goVersion}";
+      };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+
+
+          # If linking with libolm on macOS, then installing libolm via homebrew and setting
+          # these two env vars is the only way I've gotten it to work. 
+          # LIBRARY_PATH = "/opt/homebrew/lib";
+          # CPATH = "/opt/homebrew/include";
+          # If using olm via nix, then there seems to be no binary version of olm for darwin aarch64,
+          # so it tries to build it from source. But the source is 9 years old and have constructs 
+          # that does not compile with CC from nix on darwin. 
+          # What I'm currently doing, is running "go [run|build] -tags goolm ...", which seems to work OK 
+          # this far, even if the devs of mautrix don't recommend goolm for production yet.
+
+          packages = with pkgs; [
+            # go (version is specified by overlay)
+            go
+
+            # goimports, godoc, etc.
+            gotools
+
+            # https://github.com/golangci/golangci-lint
+            golangci-lint
+            golangci-lint-langserver
+            gopls
+
+            pkg-config
+
+            # Needed for crypto
+            # olm
+          ];
+        };
       });
-
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  # https://devenv.sh/reference/options/
-                  packages = with pkgs; [
-                    golangci-lint
-                    golangci-lint-langserver
-                    gopls
-                    gotools
-                  ];
-
-                  languages.go.enable = true;
-
-                  # enterShell = ''
-                  #   hello
-                  # '';
-                  #
-                  # processes.hello.exec = "hello";
-                }
-              ];
-            };
-          });
     };
 }
